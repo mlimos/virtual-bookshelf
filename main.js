115049d27d23dcff0aed1dd0e5d0f03bb6b602fe
Megan Limos <mlimos@westmonroepartners.com>
2016-09-21 09:55:41 -0700

    added imgs folder and path to new app icon in main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 900, height: 600});
+    mainWindow = new BrowserWindow({width: 900, height: 600, icon: 'imgs/books-icon.jpg'});
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
